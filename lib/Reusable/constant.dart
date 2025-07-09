0edb3782040b1a895306d40f0606c74ec6733e7c
Malareyy <suthamalar2105@gmail.com>
2025-07-09 17:21:34 +0530

    mockingtest

--- lib/Reusable/constant.dart
+++ lib/Reusable/constant.dart
@@ -1,4 +1,5 @@
 ///
 class Constants {
-  static const baseUrl = "http://192.168.18.47:5000/";
+  // static const baseUrl = "http://192.168.18.47:5000/";
+  static const baseUrl = "http://192.168.1.38:5000/";
 }
